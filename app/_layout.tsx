e05a5b5c8d7bf78d1e4f247e045e87f5fa2a6815
zahraa Meky <eng.alzahraa.meky@gmail.com>
2024-07-21 00:26:52 +0400

    edit the path of the font file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,9 @@ import { Stack } from "expo-router";
 import { useFonts } from 'expo-font';
 export default function RootLayout() {
   useFonts({
-    'Outfit': require('./assets/fonts/Outfit-Regular.ttf'),
-    'Outfit-Medium': require('./assets/fonts/Outfit-Medium.ttf'),
-    'Outfit-Bold': require('./assets/fonts/Outfit-Bold.ttf'),
+    'Outfit': require('./../assets/fonts/Outfit-Regular.ttf'),
+    'Outfit-Medium': require('./../assets/fonts/Outfit-Medium.ttf'),
+    'Outfit-Bold': require('./../assets/fonts/Outfit-Bold.ttf'),
   });
   return (
     <Stack>
