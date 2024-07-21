0b7ea3a5c7420b38db0425f6dcbfcf877b86e332
zahraa Meky <eng.alzahraa.meky@gmail.com>
2024-07-21 17:57:48 +0400

    Finish sign in screen
    In the is page we allow use r to enter his email and password to sign in or press create account if he has not and after press we redirect i to sign up page

--- app/auth/sign-in/index.js
+++ app/auth/sign-in/index.js
@@ -1,14 +1,80 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TextInput, View,TouchableOpacity} from 'react-native';
+import { useEffect } from 'react';
+import { useNavigation,useRouter } from 'expo-router';
+import { Colors } from '@/constants/Colors';
 
 const SignIn = () => {
+  const navigation = useNavigation();
+  const router = useRouter();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+  
   return (
-    <View>
-      <Text>SignIn</Text>
+    <View style={styles.mainView}>
+      <Text style={styles.paragraph}>Let's get you signed in</Text>
+      <Text style={[styles.paragraph,styles.paragraphFirst]}>Welcome Back</Text>
+      <Text style={[styles.paragraph,styles.paragraphFirst]}>You've been missed !</Text>
+
+      <View style={{marginTop:20}}>
+        <Text style={{fontFamily: 'Outfit'}}>Email</Text>
+        <TextInput style={styles.input} placeholder="Enter Email"></TextInput>
+      </View>
+
+      <View style={{marginTop:20}}>
+        <Text style={{fontFamily: 'Outfit'}}>Password</Text>
+        <TextInput style={styles.input} secureTextEntry={true} placeholder="Enter Password"></TextInput>
+      </View>
+
+      <View style={styles.button}>
+        <Text style={{textAlign:'center',color:Colors.white}}>Sign In</Text>
+      </View>
+
+      <TouchableOpacity style={[styles.button,styles.buttonInverter]}
+       onPress={()=>router.replace('auth/sign-up')}
+      >
+        <Text style={{textAlign:'center',color:Colors.primary}}>Create Account</Text>
+      </TouchableOpacity>
+      
     </View>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  mainView: {
+    backgroundColor:Colors.white,
+    height:'100%',
+    paddingTop: 80,
+    padding:25
+  },
+  paragraph: {
+    fontFamily: 'Outfit',
+    fontSize: 30,
+    marginTop: 10,
+  },
+  paragraphFirst: {
+    color: Colors.gray, 
+  },
+  input:{
+    padding:15,
+    borderWidth:1,
+    borderRadius:15,
+    borderColor:Colors.gray,
+    fontFamily: 'Outfit',
+  },
+  button: {
+    padding: 20,
+    backgroundColor: Colors.primary,
+    borderRadius:15,
+    marginTop:20
+  },
+  buttonInverter: {
+    backgroundColor: Colors.white,
+    borderWidth:1
+  },
+});
